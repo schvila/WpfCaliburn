c5ac190706100bc0d5515b5a764be8fcf73dc34b
Stanislav Chvila <schvila@gmail.com>
2020-05-25 09:45:40 +0200

    No child content controls

--- WpfCaliburn/Views/FirstChildView.xaml.cs
+++ WpfCaliburn/Views/FirstChildView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,14 +11,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfCaliburn
+namespace WpfCaliburn.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FirstChildView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FirstChildView : UserControl
     {
-        public MainWindow()
+        public FirstChildView()
         {
             InitializeComponent();
         }
